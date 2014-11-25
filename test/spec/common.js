1cedf30b8860d97fa854693a6f438e127517d9ec
Estel Smith <estel.smith@gmail.com>
2014-11-25 15:33:52 -0500

    Adding more tests.

--- test/spec/common.js
+++ test/spec/common.js
@@ -11,3 +11,60 @@ describe('Register modules', function () {
         expect(window.getDefinition('eventlib') instanceof Publisher).toBe(true);
     });
 });
+
+describe('Subscribe to topics', function () {
+    var instance = new Publisher();
+
+    it('subscribing to a new topic creates topic', function () {
+        var callback = function () {};
+
+        instance.subscribe('test_topic', callback);
+
+        expect(instance.subscribers['test_topic']).toBeDefined();
+        expect(instance.subscribers['test_topic'].length).toBe(1);
+        expect(instance.subscribers['test_topic'][0]).toBe(callback);
+    });
+
+    it('subscribing to an existing topic appends to topic', function () {
+        var callback = function () {};
+
+        instance.subscribe('test_topic', callback);
+
+        expect(instance.subscribers['test_topic'].length).toBe(2);
+        expect(instance.subscribers['test_topic'][1]).toBe(callback);
+    });
+});
+
+describe('Unsubscribe from topics', function () {
+    var instance = new Publisher();
+
+    it('cannot unsubscribe from non-existent topic', function () {
+        var callback = function () {};
+
+        expect(instance.unsubscribe('not-existent', callback)).toBe(false);
+    });
+
+    it('cannot unsubscribe from topic when not already subscribed', function () {
+        var callback = function () {};
+
+        instance.subscribe('test_topic', function () {});
+
+        expect(instance.unsubscribe('test_topic', callback)).toBe(false);
+    });
+
+    it('can unsubscribe from topic', function () {
+        var callback1 = function () {};
+        var callback2 = function () {};
+        var callback3 = function () {};
+
+        instance.subscribe('new_topic', callback1);
+        instance.subscribe('new_topic', callback2);
+        instance.subscribe('new_topic', callback3);
+
+        expect(instance.unsubscribe('new_topic', callback2)).toBe(true);
+
+        expect(instance.subscribers['new_topic'].length).toBe(2);
+        expect(instance.subscribers['new_topic'][0]).toBe(callback1);
+        expect(instance.subscribers['new_topic'][1]).toBe(callback3);
+    });
+});
